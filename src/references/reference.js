70b2940306d643f881e96e8f0e5bff759d2e2425
JuanfranMonteroMedac <jmt0003@alu.medac.es>
2022-02-14 11:46:44 +0100

    Finish project

--- src/references/reference.js
+++ src/references/reference.js
@@ -1,6 +1,6 @@
 const referenceVhc = (req, res, con) => {
     const matricula = req.query.matricula
-    let sql = `SELECT LAT,  LON FROM lista_vehiculos WHERE matricula = '${matricula}'`
+    let sql = `SELECT LAT,  LON FROM lista_vehiculos WHERE matricula = ${matricula}`
     con.query(sql,(err, result) => {
         if(err) throw err
         console.log(result);
